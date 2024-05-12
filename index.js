21c5e4737f7dc16677b71b24eed7c23541fed7a6
onodera <katenok6539@gmail.com>
2024-05-12 19:11:18 +0300

    добавление обработки нескольких чекбоксов в разных полях

--- index.js
+++ index.js
@@ -1,42 +1,61 @@
 // наценки на парфю в зависимомти от миллилитров
 let mls = {
-    0: 80,
-    1: 120,
-    2: 200,
-    3: 250,
-    4: 300
+  ml1: 80,
+  ml2: 120,
+  ml3: 200,
+  ml4: 250,
+  ml5: 300
 }
 
-let inputs = document.querySelectorAll('.ml');
-console.log(inputs);
+// достаем цифру из "итого", которую будем изменять
+let number  =  $("#all").text()
+// поскольку в теге у нас изначально строки, то нужно преобразовать наше число в цифру, чтобы оно правивльно складывалось
+number = +number
 
-// проходимся по вем из 5 чекбоксов
-  for (let i = 0; i < 6; i++) {
-    inputs[i].addEventListener('change', function() {
-      // проверяем нажатие на определенный чекбокс
-        // console.log(`Чекбокс ml1 ${this.checked ? 'выбран' : 'не выбран'}.`);
-    
-        // достаем цифру из "итого", которую будем изменять
-        let number  =  $("#all").text()
-        // поскольку в теге у нас изначально строки, то нужно преобразовать наше число в цифру, чтобы оно правивльно складывалось
-        number = +number
-        console.log(number);
-     
-    
-        // если чекбокс выбран, то прибавляем соответсвующую наценку
-        if (this.checked) {
-          console.log(`Чекбокс ${i} выбран`);
-          number += mls[i]
-          console.log(number);
-          $('#all').text(number);
-        } else {
-          console.log("Чекбокс не выбран");
-          number -= mls[i]
-          console.log(number);
-          $('#all').text(number);
-        }
-      });
+document.addEventListener('change', function(event) {
+  if (event.target.type === 'checkbox') {
+    if (event.target.checked) {
+      console.log(`Чекбокс ${event.target.id} выбран`);
+      number += mls[event.target.id]
+      console.log(number);
+      $('#all').text(number);
+    } else {
+      console.log(`Чекбокс ${event.target.id} не выбран`);
+      number -= mls[event.target.id]
+      console.log(number);
+      $('#all').text(number);
+    }
   }
+});
+
+// проходимся по всем из 5 чекбоксов
+// let inputs = document.querySelectorAll('.ml');
+// console.log(inputs);
+// for (let i = 0; i < 6; i++) {
+//   inputs[i].addEventListener('change', function() {
+//     // проверяем нажатие на определенный чекбокс
+//       // console.log(`Чекбокс ml1 ${this.checked ? 'выбран' : 'не выбран'}.`);
+  
+//       // достаем цифру из "итого", которую будем изменять
+//       let number  =  $("#all").text()
+//       // поскольку в теге у нас изначально строки, то нужно преобразовать наше число в цифру, чтобы оно правивльно складывалось
+//       number = +number
+//       console.log(number);
+   
+//       // если чекбокс выбран, то прибавляем соответсвующую наценку
+//       if (this.checked) {
+//         console.log(`Чекбокс ${i} выбран`);
+//         number += mls[i]
+//         console.log(number);
+//         $('#all').text(number);
+//       } else {
+//         console.log("Чекбокс не выбран");
+//         number -= mls[i]
+//         console.log(number);
+//         $('#all').text(number);
+//       }
+//     });
+// }
 
   // document.querySelector('#ml1').addEventListener('change', function() {
   //   console.log(`Чекбокс ml1 ${this.checked ? 'выбран' : 'не выбран'}.`);
